af23e4b8dffe0c71668226b3dbbf95f7e002e845
ButterFlakes <wksh229@proton.me>
2024-02-26 15:25:11 +0900

    [jiho] ?

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: UniversalResultScreen(),
+      home: AddUniversalFlashcardScreen(
+        categoryName: "DD",
+      ),
     );
   }
 }
